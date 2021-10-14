b1ffa6bc91858e96112a683d28b130b4010a501f
StefVercaemer <stef.vercaemer@spermalie.be>
2021-10-14 18:00:47 +0200

    Add project files.

---
+++ Prb.ClassesAndObjects.Cons/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Prb.ClassesAndObjects.Core;
+
+namespace Prb.ClassesAndObjects.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar;
+            newCar = new Car();
+
+            newCar.color = "zwart";
+            newCar.brand = "Hyundai";
+            newCar.price = 15000M;
+
+            Car secondCar;
+            secondCar = new Car();
+
+            secondCar.color = "wit";
+            secondCar.brand = "Ford";
+            secondCar.price = 9000M;
+
+            Console.WriteLine($"De nieuwe auto is een {newCar.brand}, is {newCar.color} van kleur en kost {newCar.price}");
+            Console.WriteLine("============");
+            Console.WriteLine($"De tweede auto is een {secondCar.brand}, is {secondCar.color} van kleur en kost {secondCar.price}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Prb.ClassesAndObjects.Core/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prb.ClassesAndObjects.Core
+{
+    public class Car
+    {
+        public string brand;
+        public string color;
+        public decimal price;
+    }
+}
